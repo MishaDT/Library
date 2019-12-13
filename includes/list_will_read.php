f79477b015d527010946a0b1c2e74ff43d089ce7
MishaDT <44171801+MishaDT@users.noreply.github.com>
2019-12-13 21:36:43 +0300

    Улучшение дизайна

--- includes/list_will_read.php
+++ includes/list_will_read.php
@@ -1,8 +1,7 @@
 <?php
 session_start();
 include_once 'functions.php';
-$user = new User;
-
+$user = new User();
 if (isset($_SESSION['name'])) {
     include_once 'header.php';
     $book_id = $_GET['id'];
@@ -11,9 +10,8 @@ if (isset($_SESSION['name'])) {
         <span id="button_top" onclick="return up()">&#9650; Наверх</span>
         <section class="books">
             <h1 class="title" style="margin-bottom: 50px;">Буду читать позже</h1>
-            <div class="books__list">
-                <?php $user->willRead(); // Вызов функции для вывода списка книг "читать позже" 
-                    ?>
+            <div class="books__list" id="books__list">
+                <?php $user->willRead(); ?>
             </div>
         </section>
     </div>
